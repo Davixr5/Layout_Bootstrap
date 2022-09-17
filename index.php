77833a85c1cfc60737ad56f1fe7897d1b2d5495b
Davixr5 <davijm_00@outlook.com>
2022-09-17 09:30:54 -0300

    Aula Bootstrap 16/09

---
+++ index.php
@@ -0,0 +1,12 @@
+
+<?php
+    $titulo = "Página inicial"; 
+    $migalhaPao = "Home > Usuario > Cadastro";
+    include "./cabecalho.php"; 
+?>
+
+<h1 style="color: purple;"> Aula Bootstrap </h1>
+
+<?php 
+    include "./rodape.php"; 
+?>
